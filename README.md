e1a7b9e63ecb80f7d1c9afee63e5e8f0ab4a7a5b
Picorims <picorims.contact@gmail.com>
2021-01-23 16:30:24 +0100

    attempt at fixing remixicon not found, reamde fix

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # wav2bar-website
-website for the tool Wav2Bar at https://github.com/Picorims/audio-visualizer/creator/.
+website for the tool Wav2Bar at https://github.com/Picorims/audio-visualizer-creator/.
 Visit: https://picorims.github.io/wav2bar-website
--- index.js
+++ index.js
@@ -7,9 +7,7 @@ window.onload = function() {
 
 //function to trigger animation when an element is visible. Track elements with a specific class
 function AnimateOnVisible() {
-    console.log("listening for scroll trigger.");
     var observer = new IntersectionObserver((entries) => {
-        console.log("a");
         entries.forEach(entry => {
             if (entry.intersectionRatio > 0) {
                 entry.target.classList.add('animation_trigger');
